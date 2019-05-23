e4ba733b0a72bd03d15770225b125e0b044dac4d
Guillermo <luisg1.luis@gmail.com>
2019-05-22 22:10:19 -0500

    Add Data JPA

--- src/main/java/com/bolsadeideas/springboot/datajpa/app/springbootdatajpa/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/datajpa/app/springbootdatajpa/controllers/ClienteController.java
@@ -1,10 +1,12 @@
 package com.bolsadeideas.springboot.datajpa.app.springbootdatajpa.controllers;
 
 import com.bolsadeideas.springboot.datajpa.app.springbootdatajpa.models.dao.IClienteDao;
+import com.bolsadeideas.springboot.datajpa.app.springbootdatajpa.models.entity.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ClienteController {
@@ -17,5 +19,17 @@ public class ClienteController {
         model.addAttribute("titulo","listado clientes");
         model.addAttribute("clientes",clienteDao.findAll());
         return "listar";
+    }
+    @GetMapping("/form")
+    public String crear(Model model){
+        Cliente cliente = new Cliente();
+        model.addAttribute("titulo","Fomulario de Cliente");
+        model.addAttribute("cliente",cliente);
+        return "form";
+    }
+    @PostMapping("/form")
+    public String guardar(Cliente cliente){
+        clienteDao.save(cliente);
+        return "redirect:listar";
     }
 }
